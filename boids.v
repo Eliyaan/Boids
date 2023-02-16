c6029ac27a13ad6af21ecd31ba1634ef042fef28
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-02-16 16:27:54 +0100

    Cohesion seems to work

--- boids.v
+++ boids.v
@@ -8,10 +8,10 @@ const (
     win_width    = 600
     win_height   = 600
     bg_color     = gx.black
-    nb_boids = 1000
+    nb_boids = 500
     boid_size = 2
-    speed = 5
-    detect_radius = 10
+    speed = 3
+    detect_radius = 20
     pow_detec_radius = detect_radius*detect_radius
     pow_trop_pres = 3*3
 )
@@ -48,7 +48,7 @@ fn main() {
         bg_color: bg_color
         frame_fn: on_frame
     )
-    for i in 0..nb_boids{
+    for _ in 0..nb_boids{
         app.boids << Boid{rd.int_in_range(0, win_width)!, rd.int_in_range(0, win_height)!, rd.f64_in_range(-1.0, 1.0)!, rd.f64_in_range(-1.0, 1.0)!}
     }
     //lancement du programme/de la fenêtre
@@ -59,8 +59,8 @@ fn on_frame(mut app App) {
     //Draw
     app.gg.begin()
     for mut boid in app.boids{
-        boids_trop := []Boid
-        boids_normal := []Boid
+        mut boids_trop := []Boid{}
+        mut boids_normal := []Boid{}
         //store to the right list
         if boid.x >= win_width{
             boid.x -= win_width
@@ -80,16 +80,38 @@ fn on_frame(mut app App) {
             for c in -1..2{
                 if i + l >= 0 && i + l < win_width/detect_radius && j + c >= 0 && j + c < win_height/detect_radius{
                     for other in app.opti_list[i+l][j+c]{
-                        dist := m.pow(m.abs(crea.x - other.x),2)+m.pow(m.abs(crea.y - other.y),2)
+                        dist := m.pow(m.abs(boid.x - other.x),2)+m.pow(m.abs(boid.y - other.y),2)
                         if dist < pow_detec_radius{
                             if dist < pow_trop_pres{
-                                
+                                boids_trop << other
+                            }else{
+                                boids_normal << other
                             }
                         }
                     }
                 }
             }
         }
+        // COHESION
+        mut moy_coord_x := 0.0
+        mut moy_coord_y := 0.0
+        for other in boids_trop{
+            moy_coord_x += other.x
+            moy_coord_y += other.y
+        }
+        for other in boids_normal{
+            moy_coord_x += other.x
+            moy_coord_y += other.y
+        }
+        moy_coord_x /= boids_trop.len + boids_normal.len
+        moy_coord_y /= boids_trop.len + boids_normal.len
+        boid.dir_x = moy_coord_x - boid.x
+        boid.dir_y = moy_coord_y - boid.y
+
+
+
+        boid.x += int(boid.dir_x*speed)
+        boid.y += int(boid.dir_y*speed)
 
         //draw
         app.gg.draw_circle_filled(boid.x, boid.y, boid_size, gx.red)
