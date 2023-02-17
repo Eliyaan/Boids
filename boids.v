0faf401e3acc6cfa74cb49657b8b979617f872ba
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-02-17 15:31:36 +0100

    Update boids.v

--- boids.v
+++ boids.v
@@ -5,8 +5,7 @@
 
 /*
 idées : 
--mini batches ne prendre en compte que 10 boids autour de toi ?
-
+- si y'a assez de gens dans ta subdiv, ne prendre en compte qu'eux
 
 
 */
@@ -20,7 +19,7 @@ const (
     win_width    = 640  //  /!\ the size must be un multiple du radius de détection
     win_height   = 640
     bg_color     = gx.white
-    nb_boids = 1000
+    nb_boids = 700
     boid_size = 2
     speed = 0.005
     detect_radius = 20
@@ -30,7 +29,7 @@ const (
     separation = 40
     alignement = 0.1
     friction_reduc = 0.9
-    max_crea_trop_proche = 5
+    max_crea_trop_proche = 7
 )
 
 [heap]
@@ -235,14 +234,27 @@ fn on_frame(mut app App) {
         mut delta_repoussage_x := 0.0
         mut delta_repoussage_y := 0.0
         if boids_trop.len > max_crea_trop_proche{
-            for other in boids_trop{
-                posi_cible_cohesion_x += other.x
-                posi_cible_cohesion_y += other.y
-                delta_repoussage_x += other.x
-                delta_repoussage_y += other.y
-                average_dir_x += other.dir_x
-                average_dir_y += other.dir_y
+            mut index := 0
+            for _ in 0..max_crea_trop_proche{
+                index = rd.int_in_range(0, boids_trop.len)or {panic(err)}
+                posi_cible_cohesion_x += boids_trop[index].x
+                posi_cible_cohesion_y += boids_trop[index].y
+                delta_repoussage_x += boids_trop[index].x
+                delta_repoussage_y += boids_trop[index].y
+                average_dir_x += boids_trop[index].dir_x
+                average_dir_y += boids_trop[index].dir_y
+                boids_trop.delete(index)
             }
+            posi_cible_cohesion_x /= max_crea_trop_proche
+            posi_cible_cohesion_y /= max_crea_trop_proche
+            delta_repoussage_x /= max_crea_trop_proche
+            delta_repoussage_y /= max_crea_trop_proche
+            average_dir_x /= max_crea_trop_proche
+            average_dir_y /= max_crea_trop_proche
+            delta_repoussage_x = boid.x - delta_repoussage_x
+            delta_repoussage_y = boid.y - delta_repoussage_y
+            posi_cible_cohesion_x -= boid.x
+            posi_cible_cohesion_y -= boid.y
         }else{
             for other in boids_trop{
                 posi_cible_cohesion_x += other.x
@@ -257,19 +269,18 @@ fn on_frame(mut app App) {
                 posi_cible_cohesion_y += other.y
                 average_dir_x += other.dir_x
                 average_dir_y += other.dir_y
-            }
+            }        
+            posi_cible_cohesion_x /= nb_near
+            posi_cible_cohesion_y /= nb_near
+            delta_repoussage_x /= boids_trop.len
+            delta_repoussage_y /= boids_trop.len
+            average_dir_x /= nb_near
+            average_dir_y /= nb_near
+            delta_repoussage_x = boid.x - delta_repoussage_x
+            delta_repoussage_y = boid.y - delta_repoussage_y
+            posi_cible_cohesion_x -= boid.x
+            posi_cible_cohesion_y -= boid.y
         }
-        
-        posi_cible_cohesion_x /= nb_near
-        posi_cible_cohesion_y /= nb_near
-        delta_repoussage_x /= boids_trop.len
-        delta_repoussage_y /= boids_trop.len
-        average_dir_x /= nb_near
-        average_dir_y /= nb_near
-        delta_repoussage_x = boid.x - delta_repoussage_x
-        delta_repoussage_y = boid.y - delta_repoussage_y
-        posi_cible_cohesion_x -= boid.x
-        posi_cible_cohesion_y -= boid.y
 
         boid.dir_x += average_dir_x*alignement + posi_cible_cohesion_x*cohesion + delta_repoussage_x*separation
         boid.dir_y += average_dir_y*alignement + posi_cible_cohesion_y*cohesion + delta_repoussage_y*separation
