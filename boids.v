d66b01bfd38d76e59faaab85c68ec41289d64d99
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-02-20 17:48:56 +0100

    ui bien avancée

--- boids.v
+++ boids.v
@@ -2,7 +2,7 @@
 A FAIRE:
 Bugs de couleur
 Opti
-Belle UI
+Bouton reset 
 
 
 */
@@ -16,17 +16,9 @@ const (
     win_width    = 640  //  /!\ the size must be un multiple du radius de détection
     win_height   = 640
     bg_color     = gx.white
-    nb_boids = 1000
-    boid_size = 2
-    speed = 0.003
     detect_radius = 20
     pow_detec_radius = detect_radius*detect_radius
     pow_trop_pres = 12
-    cohesion = 1
-    separation = 40
-    alignement = 0.1
-    friction_reduc = 0.9
-    max_crea_trop_proche = 10
 )
 
 [heap]
@@ -46,6 +38,15 @@ mut:
     gg    &gg.Context = unsafe { nil }
 	boids []Boid
     opti_list [][][]&Boid = [][][]&Boid{len:win_width/detect_radius, init:[][]&Boid{len:win_height/detect_radius, init:[]&Boid{cap:10}}}
+    text_cfg gx.TextCfg
+    nb_boids int = 1000
+    boid_size int = 2
+    speed f64 = 0.003
+    cohesion f64 = 1.0
+    separation f64 = 40.0
+    alignement f64 = 0.1
+    friction_reduc f64 = 0.9
+    max_crea_trop_proche int = 10
 }
 
 
@@ -64,8 +65,10 @@ fn main() {
         frame_fn: on_frame
         sample_count: 6
         fullscreen: true
+        event_fn: on_event
     )
-    for _ in 0..nb_boids{
+    app.text_cfg = gx.TextCfg{color: gx.white, size: 20, align: .left, vertical_align: .top}
+    for _ in 0..app.nb_boids{
         app.boids << Boid{rd.int_in_range(0, win_width)!, rd.int_in_range(0, win_height)!, rd.f64_in_range(-1.0, 1.0)!, rd.f64_in_range(-1.0, 1.0)!, 0.0, 0.0}
     }
     //lancement du programme/de la fenêtre
@@ -104,7 +107,7 @@ fn on_frame(mut app App) {
         mut boids_normal := []Boid{}
         i := int(boid.x/detect_radius)
         j := int(boid.y/detect_radius)
-        if app.opti_list[i][j].len < max_crea_trop_proche*1.5{
+        if app.opti_list[i][j].len < f64(app.max_crea_trop_proche)*1.5{
             for l in -1..2{
                 for c in -1..2{
                     if i + l < 0{
@@ -243,9 +246,9 @@ fn on_frame(mut app App) {
         mut average_dir_y := 0.0
         mut delta_repoussage_x := 0.0
         mut delta_repoussage_y := 0.0
-        if boids_trop.len > max_crea_trop_proche{
+        if boids_trop.len > app.max_crea_trop_proche{
             mut index := 0
-            for _ in 0..max_crea_trop_proche{
+            for _ in 0..app.max_crea_trop_proche{
                 index = rd.int_in_range(0, boids_trop.len)or {panic(err)}
                 posi_cible_cohesion_x += boids_trop[index].x
                 posi_cible_cohesion_y += boids_trop[index].y
@@ -255,12 +258,12 @@ fn on_frame(mut app App) {
                 average_dir_y += boids_trop[index].dir_y
                 boids_trop.delete(index)
             }
-            posi_cible_cohesion_x /= max_crea_trop_proche
-            posi_cible_cohesion_y /= max_crea_trop_proche
-            delta_repoussage_x /= max_crea_trop_proche
-            delta_repoussage_y /= max_crea_trop_proche
-            average_dir_x /= max_crea_trop_proche
-            average_dir_y /= max_crea_trop_proche
+            posi_cible_cohesion_x /= app.max_crea_trop_proche
+            posi_cible_cohesion_y /= app.max_crea_trop_proche
+            delta_repoussage_x /= app.max_crea_trop_proche
+            delta_repoussage_y /= app.max_crea_trop_proche
+            average_dir_x /= app.max_crea_trop_proche
+            average_dir_y /= app.max_crea_trop_proche
             delta_repoussage_x = boid.x - delta_repoussage_x
             delta_repoussage_y = boid.y - delta_repoussage_y
             posi_cible_cohesion_x -= boid.x
@@ -292,13 +295,13 @@ fn on_frame(mut app App) {
             posi_cible_cohesion_y -= boid.y
         }
 
-        boid.dir_x += average_dir_x*alignement + posi_cible_cohesion_x*cohesion + delta_repoussage_x*separation
-        boid.dir_y += average_dir_y*alignement + posi_cible_cohesion_y*cohesion + delta_repoussage_y*separation
-        boid.x += boid.dir_x*speed
-        boid.y += boid.dir_y*speed
+        boid.dir_x += average_dir_x*app.alignement + posi_cible_cohesion_x*app.cohesion + delta_repoussage_x*app.separation
+        boid.dir_y += average_dir_y*app.alignement + posi_cible_cohesion_y*app.cohesion + delta_repoussage_y*app.separation
+        boid.x += boid.dir_x*app.speed
+        boid.y += boid.dir_y*app.speed
 
-        boid.dir_x *= friction_reduc
-        boid.dir_y *= friction_reduc
+        boid.dir_x *= app.friction_reduc
+        boid.dir_y *= app.friction_reduc
 
 
         //draw
@@ -315,8 +318,129 @@ fn on_frame(mut app App) {
             blue_color += u8(boids_trop.len*20)
         }
         
-        app.gg.draw_circle_filled(f32(boid.x +50), f32(boid.y+60), boid_size, gx.Color{red_color, 0, blue_color, 255})
+        app.gg.draw_circle_filled(f32(boid.x +50), f32(boid.y+60), app.boid_size, gx.Color{red_color, 0, blue_color, 255})
     }
+    
+    //UI
+    app.gg.draw_rect_filled(800, 0, 800, 1300, gx.Color{r: 255, g: 220, b: 220})
+    app.gg.draw_text(840, 25, "Nb of boids: ${app.nb_boids}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 26, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 26, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+
+    app.gg.draw_text(840, 55, "Boids' size: ${app.boid_size}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 56, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 56, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+    
+    app.gg.draw_text(840, 85, "Boids' speed: ${app.speed}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 86, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 86, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+
+    app.gg.draw_text(840, 115, "Cohesion factor: ${app.cohesion}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 116, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 116, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+
+    app.gg.draw_text(840, 145, "Separation factor: ${app.separation}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 146, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 146, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+
+    app.gg.draw_text(840, 175, "Alignement factor: ${app.alignement}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 176, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 176, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+
+    app.gg.draw_text(840, 205, "Friction: ${app.friction_reduc}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 206, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 206, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+    
+    app.gg.draw_text(840, 235, "Boids calculated: ${app.max_crea_trop_proche}", app.text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 236, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255}) // minus
+    app.gg.draw_rounded_rect_filled(1070, 236, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255}) // plus
+
+
     app.gg.end()
     app.opti_list = [][][]&Boid{len:win_width/detect_radius, init:[][]&Boid{len:win_height/detect_radius, init:[]&Boid{cap:10}}}
+}
+
+
+
+
+fn on_event(e &gg.Event, mut app App){
+    match e.typ {
+        .key_down {
+            match e.key_code {
+                .escape {app.gg.quit()}
+                else {}
+            }
+        }
+        .mouse_up {
+            match e.mouse_button{
+                .left{app.check_buttons(e.mouse_x, e.mouse_y) or {panic("check button error")}}
+                else{}
+        }}
+        else {}
+    }
+
+}
+
+fn (mut app App) check_buttons(mouse_x f64, mouse_y f64)!{//a opti
+    if mouse_x > 1040 && mouse_x < 1090{
+        if mouse_x < 1060{
+            match true{
+                (mouse_y > 26 && mouse_y < 46){
+                        app.nb_boids -= 100
+                        app.boids = []
+                        for _ in 0..app.nb_boids{
+                            app.boids << Boid{rd.int_in_range(0, win_width)!, rd.int_in_range(0, win_height)!, rd.f64_in_range(-1.0, 1.0)!, rd.f64_in_range(-1.0, 1.0)!, 0.0, 0.0}
+                        }
+                    }
+                (mouse_y > 56 && mouse_y < 76){app.boid_size -= 1}
+                (mouse_y > 86 && mouse_y < 106){
+                        app.speed -= 0.0001
+                        app.speed = m.round_sig(app.speed, 4)
+                    }
+                (mouse_y > 116 && mouse_y < 136){app.cohesion -= 0.05
+                        app.cohesion = m.round_sig(app.cohesion, 2)
+                    }
+                (mouse_y > 146 && mouse_y < 166){app.separation -= 0.2
+                        app.separation = m.round_sig(app.separation, 1)
+                    }
+                (mouse_y > 176 && mouse_y < 196){app.alignement -= 0.05
+                        app.alignement = m.round_sig(app.alignement, 2)
+                    }
+                (mouse_y > 206 && mouse_y < 226){app.friction_reduc -= 0.05
+                        app.friction_reduc = m.round_sig(app.friction_reduc, 2)
+                    }
+                (mouse_y > 236 && mouse_y < 256){app.max_crea_trop_proche -= 1}
+                else{}
+            }
+        }else if mouse_x > 1070{
+            match true{
+                (mouse_y > 26 && mouse_y < 46){
+                        app.nb_boids += 100
+                        app.boids = []
+                        for _ in 0..app.nb_boids{
+                            app.boids << Boid{rd.int_in_range(0, win_width)!, rd.int_in_range(0, win_height)!, rd.f64_in_range(-1.0, 1.0)!, rd.f64_in_range(-1.0, 1.0)!, 0.0, 0.0}
+                        }
+                    }
+                (mouse_y > 56 && mouse_y < 76){app.boid_size += 1}
+                (mouse_y > 86 && mouse_y < 106){
+                        app.speed += 0.0001
+                        app.speed = m.round_sig(app.speed, 4)
+                    }
+                (mouse_y > 116 && mouse_y < 136){app.cohesion += 0.05
+                        app.cohesion = m.round_sig(app.cohesion, 2)
+                    }
+                (mouse_y > 146 && mouse_y < 166){app.separation += 0.2
+                        app.separation = m.round_sig(app.separation, 1)
+                    }
+                (mouse_y > 176 && mouse_y < 196){app.alignement += 0.05
+                        app.alignement = m.round_sig(app.alignement, 2)
+                    }
+                (mouse_y > 206 && mouse_y < 226){app.friction_reduc += 0.05
+                        app.friction_reduc = m.round_sig(app.friction_reduc, 2)
+                    }
+                (mouse_y > 236 && mouse_y < 256){app.max_crea_trop_proche += 1}
+                else{}
+            }
+        }
+    }
 }
